561288a7f985917bf9074b470ed90c6882863352
Calvin Sprouse <sprousecalvint@gmail.com>
2018-03-01 21:56:31 -0800

    Cleanup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExampleIterativeDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExampleIterativeDrive.java
@@ -16,11 +16,9 @@ public class ExampleIterativeDrive extends OpMode
     private DcMotor armMotor = null;
     private Servo handServo = null;
 
-    /*
-     * Code to run ONCE when the driver hits INIT
-     */
     @Override
-    public void init() {
+    public void init()
+    {
         telemetry.addData("Status:", "Initialized");
 
         leftDrive  = hardwareMap.get(DcMotor.class, "leftDrive");
@@ -40,9 +38,6 @@ public class ExampleIterativeDrive extends OpMode
         telemetry.addData("Status:", "Initialized");
     }
 
-    /*
-     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
-     */
     @Override
     public void init_loop()
     {
@@ -54,50 +49,35 @@ public class ExampleIterativeDrive extends OpMode
         telemetry.addData("Status:", "Awaiting Start");
     }
 
-    /*
-     * Code to run ONCE when the driver hits PLAY
-     */
     @Override
     public void start()
     {
         runtime.reset();
     }
 
-    /*
-     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
-     */
     @Override
     public void loop()
     {
         leftDrive.setPower(-gamepad1.left_stick_y);
         rightDrive.setPower(-gamepad1.right_stick_y);
-        armMotor.setPower((gamepad1.right_trigger/4) - (gamepad1.left_trigger/4));
+        armMotor.setPower(((gamepad1.right_trigger) - (gamepad1.left_trigger))/4);
 
-
-        try
-        {
-            handServo.setPosition(gamepad1.left_bumper ? handServo.getPosition() + 0.2 : handServo.getPosition() - 0.2);
-        }
-        catch (Exception ignored)
-        {
-            handServo.setPosition(handServo.getPosition());
-        }
+        try {handServo.setPosition(gamepad1.left_bumper ? handServo.getPosition() + 0.2 : handServo.getPosition() - 0.2);}
+        catch (Exception ignored){handServo.setPosition(handServo.getPosition());}
 
         telemetry.addData("Status:", "Run Time: " + runtime.toString());
-        telemetry.addData("Motors:", "left (%.2f), right (%.2f)", -gamepad1.left_stick_y, -gamepad1.right_stick_y);
-        telemetry.addData("Arm:", "right (%.2f), left (%.2f)", gamepad1.right_trigger, gamepad1.left_trigger);
+        telemetry.addData("Motors:", "left (%.2f), right (%.2f)", leftDrive.getPower(), rightDrive.getPower());
+        telemetry.addData("Arm:", "power (%.2f)", armMotor.getPower());
         telemetry.addData("Servo:", "position (%.2f)", handServo.getPosition());
     }
 
-    /*
-     * Code to run ONCE after the driver hits STOP
-     */
     @Override
     public void stop()
     {
         leftDrive.setPower(0.0);
         rightDrive.setPower(0.0);
         armMotor.setPower(0.0);
+        handServo.setPosition(handServo.getPosition());
 
         telemetry.addData("Status:", "Stopped");
     }
